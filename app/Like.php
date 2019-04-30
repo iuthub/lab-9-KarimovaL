7c7d34aec12cbc45c5f6332e3498151a05ee35a7
@KarimovaL <ukosimova123456@gmail.com>
2019-04-30 22:49:33 +0500

    First commit

---
+++ app/Like.php
@@ -0,0 +1,10 @@
+<?php
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+class Like extends Model
+{
+    rpublic function post()
+    {
+        return $this->belongsTo('App\Post', 'post_id');
+    }
+}
